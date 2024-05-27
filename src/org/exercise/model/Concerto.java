13626d953785b61b63d1b7437dbf8223ef414a3d
Lorenzo7991 <lorenzo1997chierisini@gmail.com>
2024-05-27 11:12:30 +0200

    Added getter methods

--- src/org/exercise/model/Concerto.java
+++ src/org/exercise/model/Concerto.java
@@ -2,14 +2,44 @@ package org.exercise.model;
 
 import java.math.BigDecimal;
 import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 
 public class Concerto extends Evento {
-    private LocalTime ora;
-    private BigDecimal prezzo;
+    //Attributi
+    private final LocalTime ora;
+    private final BigDecimal prezzo;
 
+    //Costruttore
     public Concerto(String titolo, String data, int numeroPostiTotali, LocalTime ora, BigDecimal prezzo) {
         super(titolo, data, numeroPostiTotali);
         this.ora = ora;
         this.prezzo = prezzo;
     }
+
+    //Metodi getter
+    public LocalTime getOra() {
+        return ora;
+    }
+
+    public BigDecimal getPrezzo() {
+        return prezzo;
+    }
+
+    public String getDataFormattata() {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        return getData().format(formatter);
+    }
+
+    public String getOraFormattata() {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm");
+        return ora.format(formatter);
+    }
+
+    public String getPrezzoFormattato() {
+        return String.format("%.2f€", prezzo);
+    }
+
+
+
+
 }
